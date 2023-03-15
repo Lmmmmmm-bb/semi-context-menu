f8a8068e23961caf90de453802e9851d1b3585f5
_lmmmmmm <lmmmmmm12138@gmail.com>
2023-03-16 00:56:27 +0800

    chore(playground): update example url

--- playground/src/app/config.ts
+++ playground/src/app/config.ts
@@ -1,2 +1,2 @@
 export const examplesSource =
-  'https://github.com/Lmmmmmm-bb/semi-context-menu/tree/main/playground';
+  'https://github.com/Lmmmmmm-bb/semi-context-menu/tree/main/playground/src/examples';
